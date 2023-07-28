4e7c7b29a516e7f05d55d56fcd4f6de2bbce0163
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-07-28 14:53:11 +0300

    added a div with p tags for health,damage and armor,added icons to them

--- src/components/BotItem.js
+++ src/components/BotItem.js
@@ -2,12 +2,27 @@ import React from 'react'
 
 function BotItem({bot}) {
   return (
-    <div className='bot-item'>
-        <img src={bot.avatar_url} alt={bot.name}/>
-        <h3>Name: {bot.name}</h3>
-        
+    <div className="bot-item">
+      <img src={bot.avatar_url} alt={bot.name} />
+      <h3>{bot.name}</h3>
+      <p>{bot.catchphrase}</p>
+      <p>{bot.bot_class}</p>
+      <div style={{ display: 'flex' }}>
+        <p>
+          <i className="fa-solid fa-heart-pulse"></i>
+          {bot.health}
+        </p>
+        <p>
+          <i className="fa-solid fa-bolt"></i>
+          {bot.damage}
+        </p>
+        <p>
+          <i className="fa-solid fa-shield-halved"></i>
+          {bot.armor}
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default BotItem
