bfc678f8a1c722248220dc3c1defa9cde929d0aa
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-07-28 11:24:26 +0300

    import react and react router compoments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      
+
     </div>
   );
 }
