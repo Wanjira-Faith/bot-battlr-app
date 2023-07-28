ebaf1b44d39c059ab781b9b88b979168107353e9
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-07-28 22:00:41 +0300

    mapped the bot army array to render a bot item component,added onclick event so when the user clicks on bots id handle remove from arrya is called

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import BotItem from './BotItem'
 
 function YourBotArmy({botArmy, removeFromArmy}) {
+
   return (
     <div className='army-collection'>
       <h2>Bot Army Collection</h2>
+      {botArmy.map((bot) => {
+        return (
+          <div key={bot.id} onClick={() => handleRemoveFromArray(bot)} >
+            <BotItem
+              bot={bot}
+              botArmy={botArmy}
+              removeFromArmy={removeFromArmy}
+            />
+          </div>
+        );
+      })}
     </div>
   )
 }
