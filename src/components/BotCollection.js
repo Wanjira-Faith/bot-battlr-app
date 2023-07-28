2ee8f0e2dd49439a33a648dba055f1981b5b6d70
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-07-28 12:02:14 +0300

    used use Effect hook to fetch data on initial render

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,7 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function BotCollection() {
-    const [bots, setBots] = useState([])
+    const [bots, setBots] = useState([]);
+
+    useEffect(() => {
+        fetch(" http://localhost:3000/bots")
+        .then((r) => r.json())
+        .then((bots) => setBots(bots))
+    }, [])
   return (
     <div>
       
