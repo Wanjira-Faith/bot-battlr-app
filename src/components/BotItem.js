30f813da3d52f48b93a4b05a8b979bc48157fce5
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-07-28 12:42:09 +0300

    added image avatar url

--- src/components/BotItem.js
+++ src/components/BotItem.js
@@ -3,10 +3,9 @@ import React from 'react'
 function BotItem({bot}) {
   return (
     <div className='bot-item'>
+        <img src={bot.avatar_url} alt={bot.name}/>
         <h3>Name: {bot.name}</h3>
-
-
-      
+        
     </div>
   )
 }
