40826b5c7613e0e2e8d156905f983be5025b7256
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-07-28 21:34:22 +0300

    added classname and h2 tag

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function YourBotArmy({botArmy, removeFromArmy}) {
   return (
-    <div>
-      
+    <div className='army-collection'>
+      <h2>Bot Army Collection</h2>
     </div>
   )
 }
