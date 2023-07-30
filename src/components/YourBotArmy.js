4a1e26496df27e6e9fd9748afe794fc6b5957c16
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-07-30 16:21:52 +0300

    changed h2 tag heading

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -9,7 +9,9 @@ function YourBotArmy({ botArmy, removeFromArmy}) {
 
   return (
     <>
-      <h2 className="army-header">Bot Army Collection</h2>
+      <h2 className="army-header">
+        Click on a Bot on the bot collection to enlist it to your army.
+      </h2>
       <div className="army-collection">
         {botArmy.map((bot) => (
           <div key={bot.id} onClick={() => handleRemoveFromArmy(bot)}>
@@ -18,7 +20,7 @@ function YourBotArmy({ botArmy, removeFromArmy}) {
             <p>{bot.catchphrase}</p>
             <p>Class: {bot.bot_class}</p>
             <hr />
-            <div className='icons'>
+            <div className="icons">
               <p>
                 <i className="fa-solid fa-heart-pulse"></i>
                 {bot.health}
