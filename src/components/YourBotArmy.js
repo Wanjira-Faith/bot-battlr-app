064e8a1f54bc26ae025ddfb2dd444e410d179b5f
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-07-30 12:46:22 +0300

    adjusted h2 and added classname

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,28 +1,30 @@
 import React from 'react'
 import BotItem from './BotItem'
 
-function YourBotArmy({botArmy, removeFromArmy}) {
-
-   function handleRemoveFromArmy(bot) {
-     removeFromArmy(bot);
-   }
+function YourBotArmy({ botArmy, removeFromArmy, addBotToArmy }) {
+  function handleRemoveFromArmy(bot) {
+    removeFromArmy(bot);
+  }
 
   return (
-    <div className='army-collection'>
-      <h2>Bot Army Collection</h2>
-      {botArmy.map((bot) => {
-        return (
-          <div key={bot.id} onClick={() => handleRemoveFromArmy(bot)} >
-            <BotItem
-              bot={bot}
-              botArmy={botArmy}
-              removeFromArmy={removeFromArmy}
-            />
-          </div>
-        );
-      })}
-    </div>
-  )
+    <>
+      <h2 className="army-header">Bot Army Collection</h2>
+      <div className="army-collection">
+        {botArmy.map((bot) => {
+          return (
+            <div key={bot.id} onClick={() => handleRemoveFromArmy(bot)}>
+              <BotItem
+                bot={bot}
+                botArmy={botArmy}
+                removeFromArmy={removeFromArmy}
+                addBotToArmy={addBotToArmy}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default YourBotArmy
